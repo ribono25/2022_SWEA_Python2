240e28a105023256da25d4a67346d562c28f1b2d
SeongHyeon Kim <94503162+ribono25@users.noreply.github.com>
2022-03-09 02:11:35 +0900

    Update module_10.py

--- 01_모듈과 패키지(예제)/module_10.py
+++ 01_모듈과 패키지(예제)/module_10.py
@@ -5,21 +5,21 @@ from random import sample
 def input_start():
     try:
         start_number = int(input('로또 번호의 시작 번호를 입력하세요 (기본값: 1): '))
-    except Exception as ValueError:
+    except ValueError:
         start_number = 1
     finally:
         return start_number
 def input_end():
     try:
         end_number = int(input('로또 번호의 끝 번호를 입력하세요 (기본값: 45): '))
-    except Exception as ValueError:
+    except ValueError:
         end_number = 45
     finally:
         return end_number
 def input_amount():
     try:
         amount = int(input('로또 공의 개수를 입력하세요 (기본값: 6): '))
-    except Exception as ValueError:
+    except ValueError:
         amount = 6
     finally:
         return amount
